b37f2d9dc1314afee67d6bac37f3196db8b39553
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2022-07-25 14:42:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,188 @@
+let btnHasBeen = document.getElementById('btnHasBeen')
+
+btnHasBeen.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnHasBeen').innerHTML = 'sempre foi'
+        document.getElementById('btnHasBeen').setAttribute("style", "box-shadow: 0 0 0")
+        btnHasBeen.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnHasBeen').innerHTML = "has always been"
+            document.getElementById('btnHasBeen').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnHasBeen.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnTrouble = document.getElementById('btnTrouble')
+
+btnTrouble.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnTrouble').innerHTML = 'se mete em encrenca'
+        document.getElementById('btnTrouble').setAttribute("style", "box-shadow: 0 0 0")
+        btnTrouble.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnTrouble').innerHTML = "gets into trouble"
+            document.getElementById('btnTrouble').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnTrouble.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnAsked = document.getElementById('btnAsked')
+
+btnAsked.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnAsked').innerHTML = 'pediu que ele fosse'
+        document.getElementById('btnAsked').setAttribute("style", "box-shadow: 0 0 0")
+        btnAsked.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnAsked').innerHTML = "asked him to go"
+            document.getElementById('btnAsked').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnAsked.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnWay = document.getElementById('btnWay')
+
+btnWay.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnWay').innerHTML = 'a caminho da'
+        document.getElementById('btnWay').setAttribute("style", "box-shadow: 0 0 0")
+        btnWay.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnWay').innerHTML = "On his way to the"
+            document.getElementById('btnWay').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnWay.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnWhatIf = document.getElementById('btnWhatIf')
+
+btnWhatIf.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnWhatIf').innerHTML = '"E se eu começasse a cultivar'
+        document.getElementById('btnWhatIf').setAttribute("style", "box-shadow: 0 0 0")
+        btnWhatIf.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnWhatIf').innerHTML = '"What if I started growing'
+            document.getElementById('btnWhatIf').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnWhatIf.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnWouldntNeed = document.getElementById('btnWouldntNeed')
+
+btnWouldntNeed.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnWouldntNeed').innerHTML = 'eu não precisaria'
+        document.getElementById('btnWouldntNeed').setAttribute("style", "box-shadow: 0 0 0")
+        btnWouldntNeed.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnWouldntNeed').innerHTML = "I wouldn't need"
+            document.getElementById('btnWouldntNeed').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnWouldntNeed.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnOwn = document.getElementById('btnOwn')
+
+btnOwn.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnOwn').innerHTML = 'suas próprias'
+        document.getElementById('btnOwn').setAttribute("style", "box-shadow: 0 0 0")
+        btnOwn.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnOwn').innerHTML = 'his own'
+            document.getElementById('btnOwn').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnOwn.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnWentInto = document.getElementById('btnWentInto')
+
+btnWentInto.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnWentInto').innerHTML = 'entrou'
+        document.getElementById('btnWentInto').setAttribute("style", "box-shadow: 0 0 0")
+        btnWentInto.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnWentInto').innerHTML = 'went into'
+            document.getElementById('btnWentInto').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnWentInto.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnBought = document.getElementById('btnBought')
+
+btnBought.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnBought').innerHTML = 'comprou tudo'
+        document.getElementById('btnBought').setAttribute("style", "box-shadow: 0 0 0")
+        btnBought.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnBought').innerHTML = 'bought everything'
+            document.getElementById('btnBought').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnBought.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnWentBack = document.getElementById('btnWentBack')
+
+btnWentBack.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnWentBack').innerHTML = 'voltou pra casa'
+        document.getElementById('btnWentBack').setAttribute("style", "box-shadow: 0 0 0")
+        btnWentBack.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnWentBack').innerHTML = 'went back home'
+            document.getElementById('btnWentBack').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnWentBack.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+
+let btnPlanning = document.getElementById('btnPlanning')
+
+btnPlanning.addEventListener('click', function() {
+    let audio = document.getElementById('clickSound')
+    audio.play()
+    
+        document.getElementById('btnPlanning').innerHTML = 'os planos para fazer'
+        document.getElementById('btnPlanning').setAttribute("style", "box-shadow: 0 0 0")
+        btnPlanning.style.backgroundColor = 'lime'
+        setTimeout(function() {
+            document.getElementById('btnPlanning').innerHTML = 'planning on making'
+            document.getElementById('btnPlanning').setAttribute("style", "box-shadow: 1px 1px 2px")
+            btnPlanning.style.backgroundColor = 'white'
+        }, 2000)
+    }
+)
+   
+    
